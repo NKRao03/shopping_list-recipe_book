b51f061e7caf2914db82af12c9ea707ef51a742d
Nandakishore Rao <36339632+NKRao03@users.noreply.github.com>
2022-12-25 19:35:01 +0530

    Worked on rearranging components, deleted redundant spec files

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +6,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes : Recipe[] =[ 
     new Recipe('Sample Recipe','This is a sample recipe', 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjaG2cfLMIULCNfKLV82l_8K1f7iX_Go-85M_5eCTDxvExQpgNl4s7UWq4YMrsJmI3SURRuGhV7Luu9uHFB53QAu2WnW9E-FN0ollJHgNMMBEjJz2FGuzfFQfBKV5LFWzPztxjgSM9DtPpKrr-qrPlMQJrGwkvEcMxEXeUZSLMBW3_WIR73p-UO4gU30Q/s1280/cheese-garlic-bread-min.jpg')
     ];
@@ -14,6 +15,10 @@ export class RecipeListComponent implements OnInit {
   }
   
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe:Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static:false}) nameInputRef:ElementRef;
+  @ViewChild('quantityInput', {static:false}) quantitiyInputRef:ElementRef;
+
+  @Output() sendDetails = new EventEmitter<Ingredient>();
+
+  onSelection() {
+    this.sendDetails.emit(new Ingredient(this.nameInputRef.nativeElement.value, this.quantitiyInputRef.nativeElement.value));
+  }
 
   constructor() { }
 
